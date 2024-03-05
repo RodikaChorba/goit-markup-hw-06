2590329f0733072ac09ae1fdf7a05da36e30f2a9
Rodika Chorba <158561902+RodikaChorba@users.noreply.github.com>
2024-03-05 20:15:39 +0100

    finish

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openModal() {
+    document.querySelector('.backdrop').classList.add('is-open');
+}
+function closeModal() {
+    document.querySelector('.backdrop').classList.remove('is-open');
+}
